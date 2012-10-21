1ac13997c267ae8d8afd108430bfb8628b4e883b
Field Office <hello@fieldoffice.co.uk>
2012-10-21 17:39:40 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	
+	<title>Site Title</title>
+	
+	<!-- META -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="" />
+  	<meta name="robots" content="index, follow" />
+	<meta name="author" content="" />
+	<meta name="HandheldFriendly" content="true" />  
+	
+	<link rel="canonical" href="" />
+	<link rel="author" type="text/plain" href="humans.txt" />
+
+	<!-- Favicons -->
+	<link rel="shortcut icon" href="/assets/favicons/16.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/favicons/57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/favicons/72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/favicons/114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/favicons/144.png" />
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="/assets/css/laika.css" media="screen" />
+	<link rel="stylesheet" href="/assets/css/laika-print.css" media="print" />
+
+	<!-- WEB FONTS -->
+        
+	<!-- IE CONDITIONALS -->
+	<!--[if lt IE 9]>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+
+<!--[if IE]><body class="ie"><![endif]-->
+<!--[if !IE]>--><body><!--<![endif]-->
+
+<!-- HEADER START -->
+
+<ul id="skiplinks">
+	<li><a href="#main" class="visuallyhidden focusable">Skip to main content</a></li>
+	<li><a href="#primary-nav" class="visuallyhidden focusable">Skip to navigation</a></li>
+</ul>
+
+<header class="header" role="banner">
+
+	<nav role="navigation">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Site Section</a></li>
+			<li><a href="#">Site Section</a></li>
+		</ul>
+	</nav>
+
+</header>
+
+<!-- HEADER END -->
+
+<!-- CONTENT START -->
+
+<div class="main" role="main">
+
+</div><!-- /.main -->
+
+<!-- CONTENT END -->
+
+<!-- FOOTER START -->
+
+<footer class="footer" role="contentinfo">
+
+</footer>
+
+<!-- FOOTER END -->
+
+<!-- JS -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/assets/js/jquery-1.7.2.min.js"><\/script>')</script>
+<script src="/assets/js/script.js"></script>
+
+<!-- ANALYTICS -->
+
+<script>
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
+</body>
+</html>
